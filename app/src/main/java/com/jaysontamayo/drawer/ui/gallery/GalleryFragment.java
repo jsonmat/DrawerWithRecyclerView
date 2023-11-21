97fe059b9bbbe9662fb4aebd914c8022a59bcf4f
Jayson Tamayo <jayson.tamayo2@gmail.com>
2023-11-21 20:01:17 +0800

    first commit

---
+++ app/src/main/java/com/jaysontamayo/drawer/ui/gallery/GalleryFragment.java
@@ -0,0 +1,42 @@
+package com.jaysontamayo.drawer.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jaysontamayo.drawer.R;
+import com.jaysontamayo.drawer.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        CustomAdapter customAdapter = new CustomAdapter(new String[]{"Jayson", "Tamayo"});
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setAdapter(customAdapter);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
